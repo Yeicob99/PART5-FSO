f83ac2782ce005adef8e5c86c1fcfdc2d78fec4d
Jacobo <yosoyjacobo23@gmail.com>
2025-05-08 21:13:49 -0700

    Exercise 5.7 done

--- src/components/CreateBlogsForm.jsx
+++ src/components/CreateBlogsForm.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
 
 const CreateBlogsForm = ({ createBlog }) => {
-
   const [title, setTitle] = useState("");
   const [author, setAuthor] = useState("");
   const [url, setUrl] = useState("");
+  const [blogsVisible, setBlogsVisible] = useState(false);
+
+  const hideWhenVisible = { display: blogsVisible ? "none" : "" };
+  const showWhenVisible = { display: blogsVisible ? "" : "none" };
 
   const submit = (event) => {
     event.preventDefault();
@@ -15,31 +18,39 @@ const CreateBlogsForm = ({ createBlog }) => {
   };
 
   return (
-    <form onSubmit={submit}>
-      <h3>Create new Blog</h3>
-      <div>
-        Title:
-        <input
-          value={title}
-          onChange={({ target }) => setTitle(target.value)}
-        />
+    <div>
+      <div style={hideWhenVisible}>
+        <button onClick={() => setBlogsVisible(true)}>Create Blogs</button>
       </div>
-      <div>
-        Author:
-        <input
-          value={author}
-          onChange={({ target }) => setAuthor(target.value)}
-        />
+      <div style={showWhenVisible}>
+        <button onClick={() => setBlogsVisible(false)}>Hide Blogs</button>
+        <form onSubmit={submit}>
+          <h3>Create new Blog</h3>
+          <div>
+            Title:
+            <input
+              value={title}
+              onChange={({ target }) => setTitle(target.value)}
+            />
+          </div>
+          <div>
+            Author:
+            <input
+              value={author}
+              onChange={({ target }) => setAuthor(target.value)}
+            />
+          </div>
+          <div>
+            URL:
+            <input
+              value={url}
+              onChange={({ target }) => setUrl(target.value)}
+            />
+          </div>
+          <button type="submit">Create</button>
+        </form>
       </div>
-      <div>
-        URL:
-        <input
-          value={url}
-          onChange={({ target }) => setUrl(target.value)}
-        />
-      </div>
-      <button type="submit">Create</button>
-    </form>
+    </div>
   );
 };
 
