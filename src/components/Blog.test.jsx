e5c7928014f8acf10f4da138311e3fba251438ec
Jacobo <yosoyjacobo23@gmail.com>
2025-05-15 22:08:24 -0700

    exercise 5.13 done

--- src/components/Blog.test.jsx
+++ src/components/Blog.test.jsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react'
 import {vi} from 'vitest'
 import Blog from './Blog'
+import { fireEvent } from '@testing-library/react'
 
 test('renders title and author, but not url or likes by default', () => {
     const blog = {
@@ -20,12 +21,12 @@ test('renders title and author, but not url or likes by default', () => {
     const title = screen.getByText('Test Blog')
     expect(title).toBeDefined()
 
+    expect(screen.queryByText('https://testblog.com')).toBeNull()
+    expect(screen.queryByText('Likes: 10')).toBeNull()
 
+    const button = screen.getByText('View Details')
+    fireEvent.click(button)
 
-    const url = screen.queryByText('https://testblog.com')
-    const likes = screen.queryByText('Likes: 10')
-    const author = screen.queryByText('Test Author')
-    expect(url).toBeNull()
-    expect(likes).toBeNull()
-
+    expect(screen.getByText('https://testblog.com')).toBeDefined()
+    expect(screen.getByText('Likes: 10')).toBeDefined()
 })
